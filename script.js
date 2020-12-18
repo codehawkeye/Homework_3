232f5c9c9245c1be1e49d4d461acfce8a0305f40
Mike Carter <elite.mikecarter@gmail.com>
2020-12-18 18:39:39 -0500

    added return statment to correct console log incase multiple bad

--- script.js
+++ script.js
@@ -90,11 +90,116 @@ var generateBtn = document.querySelector("#generate");
 
   // Add event listener to generate button
   generateBtn.addEventListener("click", writePassword);
- 
+// user input varibles
+var length
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
 
- 
-   
-   
 
+// Write password to the #password input
+function writePassword() {
+//  Ask for user input
+var options = getPassWordOptions(); 
+
+function getPassWordOptions() {
+   length = parseInt(prompt(" Please enter desired password Length 8-128"));
+  // conditional statment to ensure user input
+}
+  if (!length) {
+    alert("Press Generate to begin password process");
+    return
   
+  } else if (length < 8 || length > 128) {
+    length = parseInt(prompt("You must choose between 8 and 128"));
+    // validate user input
+  
+
+  } else {
+     // Continues once user input is validated
+     confirmNumber = confirm("Will this contain numbers?");
+     confirmCharacter = confirm("Will this contain special characters?");
+     confirmUppercase = confirm("Will this contain Uppercase letters?");
+     confirmLowercase = confirm("Will this contain Lowercase letters?");
+ }; 
+
+
+    // Else if for 4 negative options
+    if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+      masterArray = alert("You must choose a criteria!");
+
+  }
+  // First if statement that uses user input prompts to determine masterArray
+  // Else if for 4 positive options
+  else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+
+      masterArray = specialCharacters.concat(numericCharacters, lowerCasedCharacters, upperCasedCharacters);
+  }
+  // Else if for 3 positive options
+  else if (confirmCharacter && confirmNumber && confirmUppercase) {
+      masterArray = specialCharacters.concat(numericCharacters, lowerCasedCharacters);
+  }
+  else if (confirmCharacter && confirmNumber && confirmLowercase) {
+      masterArray = specialCharacters.concat(numericCharacters,lowerCasedCharacters);
+  }
+  else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+      masterArray = specialCharacters.concat(lowerCasedCharacters, upperCasedCharacters);
+  }
+  else if (confirmNumber && confirmLowercase && confirmUppercase) {
+      masterArray = numericCharacters.concat(lowerCasedCharacters, upperCasedCharacters);
+  }
+  // Else if for 2 positive options 
+  else if (confirmCharacter && confirmNumber) {
+      masterArray = specialCharacters.concat(numericCharacters);
+
+  } else if (confirmCharacter && confirmLowercase) {
+      masterArray = specialCharacters.concat(lowerCasedCharacters);
+
+  } else if (confirmCharacter && confirmUppercase) {
+      masterArray = specialCharacters.concat(upperCasedCharacters);
+  }
+  else if (confirmLowercase && confirmNumber) {
+      masterArray = lowerCasedCharacters.concat(numericCharacters);
+
+  } else if (confirmLowercase && confirmUppercase) {
+      masterArray = lowerCasedCharacters.concat(upperCasedCharacters);
+
+  } else if (confirmNumber && confirmUppercase) {
+      masterArray = numericCharacters.concat(upperCasedCharacters);
+  }
+  // Else if for 1 positive option
+  else if (confirmCharacter) {
+      masterArray = specialCharacters;
+  }
+  else if (confirmNumber) {
+      masterArray = numericCharacters;
+  }
+  else if (confirmLowercase) {
+      masterArray = lowerCasedCharacters;
+  }
+    else if (confirmUppercase) {
+      masterArray = upperCasedCharacters;
+};
+  // password variable is an array placeholder for user generated amount of length ( stores array)
+  var password = [];
+
+  // Start random selection variables:
+  // Random selection for all variables: 
+  for (var i = 0; i < length; i++) {
+      var pickChoices = masterArray[Math.floor(Math.random() * masterArray.length)];
+      password.push(pickChoices);
+  }
+  // This joins the password array and converts it to a string  
+  var ps = password.join("");
+  UserInput(ps);
+  return ps;
+  
+}
+// Changed function input to use textcontent
+function UserInput(ps) {
+  document.getElementById("password").textContent = ps;
+
+}
+
 
